7d7d5cda188e6102251b0cefda1e76e26e97fa82
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-09-12 13:53:11 -0400

    Update SampleController.java

--- src/main/java/com/co/services/sample/controller/SampleController.java
+++ src/main/java/com/co/services/sample/controller/SampleController.java
@@ -1,14 +1,16 @@
 package com.co.services.sample.controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class SampleController {
-    
+    private static final Logger LOGGER = LoggerFactory.getLogger(SampleController.class);
+
     @GetMapping("/")
     public String sampleApi() {
-
+        LOGGER.info("Sample 5 Called");
         return "Hello Sample Microservice 5";
     }
     
